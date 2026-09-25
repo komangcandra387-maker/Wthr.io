1835beefa8ea5b3f36b1540d208907e4cfc316f6
komangcandra387-maker <komangcandra387@gmail.com>
2026-09-25 19:39:25 +0800

    Delete HelperClass.kt

--- HelperClass.kt
+++
@@ -1,7 +0,0 @@
-package com.fauzan.wthrio
-class HelperClass {
-    fun updateState(isChecked: Boolean) {
-        // Update state di Singleton
-        StateManager.switchState = isChecked
-    }
-}
