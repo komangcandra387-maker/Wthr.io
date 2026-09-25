1719c334cc24adf26c870b8676bf764b264f2cdf
komangcandra387-maker <komangcandra387@gmail.com>
2026-09-25 19:42:27 +0800

    Create SecondActivity.kt

---
+++ app/src/main/java/com/candra/wthrio/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.candra.wthrio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+		
+		
+    }
+}
